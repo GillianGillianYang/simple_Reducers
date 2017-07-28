943b430568f2c35b7b96e4a512e2653b779dcf96
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-07-28 14:42:27 +0800

    39 reducers
    reducers :

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { term: 'javscript : the good parts' },
+        { term: 'harry porter' },
+        { term: 'the dark tower' },
+        { term: 'eloquent ruby' }
+    ];
+}
