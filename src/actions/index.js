59a157349bfdc74d537d48d50bc602f6d441bc78
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-07-28 17:33:39 +0800

    43,44,45 action creator
    43containers and reducers review
    good review fro the container and reducer
    
    44actions and action creators
    …code workflow  explain…
    
    45.binding action creators
    book-list.js:
    import {connect} from ‘react-redux’
    turns booklist form a component to a container
    connect function : connect booklist and state

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+//action creator
+
+export function selectBook(book) {
+    console.log('A book has been selected:', book.title);
+}
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { connect } from 'react-redux' //connect the container with the state 
+import { selectBook } from '../actions/index'; // action creator 
+import { bindActionCreators } from 'redux'; //bind action creator  make sure it go through all the reducer
+
 
 //export default
 class BookList extends Component {
@@ -37,4 +40,18 @@ function mapStateToProps(state) {
     return { books: state.books };
 }
 
-export default connect(mapStateToProps)(BookList);
+//anything return from this function will end up as props
+//on the BookList container
+function mapDispatchToProps(dispatch) {
+    //Whenever selectBook is called, the result should be passed to all of ours reducers
+    return
+    bindActionCreators({ selectBook: selectBook }, dispatch);
+    // selectBook -> this.props.selectBook
+}
+
+
+//Promote BookList from a component to a container - it needs to 
+//know about this new dispatch method,selectBook. Make it available as a prop.
+
+//export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
